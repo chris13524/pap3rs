c1e64639c68665c3d7c5848713fa29c9f5ec7803
Chris Smith <chris@dtllc.io>
2022-07-16 19:22:46 -0400

    lint

--- pages/donate.tsx
+++ pages/donate.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 import { useAccount, useSigner } from "wagmi";
-import { usePapersContract, useMockTokenContract } from '../utils/contracts';
+import { usePapersContract, useMockTokenContract } from "../utils/contracts";
 
 async function approve(contract,cid) {
   console.log(`Awaiting claim call for cid: ${cid}`);
@@ -13,33 +13,33 @@ async function approve(contract,cid) {
 
 const Upload: NextPage = () => {
 
-    const { data: signer, isError: isError2, isLoading: isLoading2 } = useSigner();
-    const { data: account, isError, isLoading, address, isConnected } = useAccount();
+  const { data: signer, isError: isError2, isLoading: isLoading2 } = useSigner();
+  const { data: account, isError, isLoading, address, isConnected } = useAccount();
 
-    const contract = usePapersContract(signer);
-    console.log('signer',signer);
-    console.log('contract',contract);
-    const mockTokenContract = useMockTokenContract(signer);
-    console.log('mockTokenContract',mockTokenContract);
+  const contract = usePapersContract(signer);
+  console.log("signer",signer);
+  console.log("contract",contract);
+  const mockTokenContract = useMockTokenContract(signer);
+  console.log("mockTokenContract",mockTokenContract);
 
-    let donationAmount = 100;
-    let cid = 'bafybeihwynhkv3kkxi7snayboj66vfyqa73wp6uxshogsedwfjoduizgmy';
+  let donationAmount = 100;
+  let cid = "bafybeihwynhkv3kkxi7snayboj66vfyqa73wp6uxshogsedwfjoduizgmy";
 
-    async function approve() {
-      try {
-          let tokenName = await mockTokenContract.name();
-          console.log(`Going to approve ${donationAmount} ${tokenName}`);
-          await mockTokenContract.approve(contract.address, `${donationAmount}000000000000000000`);
-      }
-      catch(error) {
-          console.error('Error approving: ',error)
-      }
+  async function approve() {
+    try {
+      let tokenName = await mockTokenContract.name();
+      console.log(`Going to approve ${donationAmount} ${tokenName}`);
+      await mockTokenContract.approve(contract.address, `${donationAmount}000000000000000000`);
     }
-
-    async function donate() {
-      console.log(`Going to donate ${donationAmount} to CID ${cid} via contract: ${contract.address}`);
-      await contract.donate(cid, `${contract.address}`, `${donationAmount}000000000000000000`);
+    catch(error) {
+      console.error("Error approving: ",error);
     }
+  }
+
+  async function donate() {
+    console.log(`Going to donate ${donationAmount} to CID ${cid} via contract: ${contract.address}`);
+    await contract.donate(cid, `${contract.address}`, `${donationAmount}000000000000000000`);
+  }
 
   return (
     <div className={styles.container}>
@@ -54,23 +54,23 @@ const Upload: NextPage = () => {
 
         <p className={styles.description}>
         cid
-        <input type="text" value={cid}/>
+          <input type="text" value={cid}/>
         donationAmount
-        <input type="text" value={donationAmount}/>
-        <button
-          className="btn btn-warning"
-          type="button"
-          onClick={approve}
-        >
+          <input type="text" value={donationAmount}/>
+          <button
+            className="btn btn-warning"
+            type="button"
+            onClick={approve}
+          >
           Approve {donationAmount}
-        </button>
-        <button
-          className="btn btn-warning"
-          type="button"
-          onClick={donate}
-        >
+          </button>
+          <button
+            className="btn btn-warning"
+            type="button"
+            onClick={donate}
+          >
           Donate {donationAmount}
-        </button>
+          </button>
         </p>
       </main>
     </div>
