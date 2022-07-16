f07500eca7d93351190d68d4edbebad9729839b7
Chris Smith <chris@smith.xyz>
2022-07-16 19:22:46 -0400

    lint

--- components/navlinks.tsx
+++ components/navlinks.tsx
@@ -7,30 +7,30 @@ type Link = {
 };
 
 const NavLink = ({ href, label }: Link) => {
-    return <UnstyledButton component={NextLink} href={href} sx={(theme) => ({
-        display: 'block',
-        width: '100%',
-        padding: theme.spacing.xs,
-        borderRadius: theme.radius.sm,
-        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
-        '&:hover': {
-            backgroundColor:
-                theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-        },
-    })}>
-        <Text>{label}</Text>
-    </UnstyledButton>;
+  return <UnstyledButton component={NextLink} href={href} sx={(theme) => ({
+    display: "block",
+    width: "100%",
+    padding: theme.spacing.xs,
+    borderRadius: theme.radius.sm,
+    color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
+    "&:hover": {
+      backgroundColor:
+                theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
+    },
+  })}>
+    <Text>{label}</Text>
+  </UnstyledButton>;
 };
 
 const links: Link[] = [
-    { href: "/", label: "Home" },
-    { href: "/paper/abc", label: "View paper" },
-    { href: "/upload", label: "Upload paper" },
-    { href: "/donate", label: "Donate" },
+  { href: "/", label: "Home" },
+  { href: "/paper/abc", label: "View paper" },
+  { href: "/upload", label: "Upload paper" },
+  { href: "/donate", label: "Donate" },
 ];
 
 const NavLinks = ({ }) => {
-    return <>{links.map(link => <NavLink {...link} key={link.label} />)}</>;
+  return <>{links.map(link => <NavLink {...link} key={link.label} />)}</>;
 };
 
 export default NavLinks;
