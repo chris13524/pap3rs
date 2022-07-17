b491ae23c274d16d04571f945fd207f9365f48e7
Chris Smith <chris@smith.xyz>
2022-07-16 21:46:39 -0400

    Add text

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,8 @@ type Paper = {
 };
 
 const featuredPapers: Paper[] = [
-  { cid: "bafybeihwynhkv3kkxi7snayboj66vfyqa73wp6uxshogsedwfjoduizgmy", title: "decentralized papers", description: "my desc" },
-  { cid: "bafybeihwynhkv3kkxi7snayboj66vfyqa73wp6uxshogsedwfjoduizgmy", title: "decentralized papers 2", description: "my desc" },
+  { cid: "bafybeihwynhkv3kkxi7snayboj66vfyqa73wp6uxshogsedwfjoduizgmy", title: "Towards a Decentralized Process for Scientific Publication and Peer Review using Blockchain and IPFS", description: "The current processes of scientific publication and peer review raise concerns around fairness, quality, performance, cost, and accuracy. The Open Access movement has been unable to fulfill all its promises, and a few middlemen publishers can still impose policies and concentrate profits. This paper, using emerging distributed technologies such as Blockchain and IPFS, proposes a decentralized publication system for open science. The proposed system would provide (1) a distributed reviewer reputation system, (2) an Open Access by-design infrastructure, and (3) transparent governance processes. A survey is used to evaluate the problems, proposed solutions and possible adoption resistances, while a working prototype serves as a proof-of-concept. Additionally, the paper discusses the implementation, in a distributed context, of different privacy settings for both open peer review and reputation systems, introducing a novel approach supporting both anonymous and accountable reviews. The paper concludes reviewing the open challenges of this ambitious proposal." },
+  { cid: "bafybeihwynhkv3kkxi7snayboj66vfyqa73wp6uxshogsedwfjoduizgm2", title: "decentralized papers 2", description: "my desc" },
 ];
 
 const Home: NextPage = () => {
@@ -32,10 +32,10 @@ const Home: NextPage = () => {
 
       <Grid>
         {featuredPapers.map(paper => (
-          <Grid.Col md={6} lg={4}>
+          <Grid.Col md={6} lg={4} key={paper.cid}>
             <Paper shadow="xs" p="md" component={NextLink} href={`/paper/${paper.cid}`}>
-              <Title order={2}>{paper.title}</Title>
-              <Text>{paper.description}</Text>
+              <Title order={4}>{paper.title}</Title>
+              <Text lineClamp={4}>{paper.description}</Text>
             </Paper>
           </Grid.Col>
         ))}
