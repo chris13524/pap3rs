cc6cf462c85418c77fa0451c0569dbc4fccbe257
Chris Smith <chris@dtllc.io>
2022-07-23 00:35:39 -0400

    Add author viewing page

--- utils/paper.ts
+++ utils/paper.ts
@@ -1,3 +1,5 @@
+import { retrieveJson } from "./ipfs";
+
 export type Paper = {
   authors: string[],
   title: string,
@@ -7,8 +9,19 @@ export type Paper = {
   references: string[],
 };
 
+export type PaperWithId = Paper & { id: string };
+
 export const allPapers = [
   "bafybeia2mm2clf3wnkwqlakjyhichnakuwn2fn2c4ycktxzmxa3qjanqou",
   "bafybeidugsonwrnlr5wfrmi5r7beqkhx6pphegu5kgn4eeqwrdf7e3mw6y",
   "bafybeib7d2fya6d2fjhsq7mbzaeksnw7jkhlzbmk3s6zskmced6xuies7a",
 ];
+
+export async function getPapers(cids: string[]): Promise<PaperWithId[]> {
+  const papers = [];
+  for (const cid of cids) {
+    const paper = Object.assign(await retrieveJson<PaperWithId>(cid), { id: cid });
+    papers.push(paper);
+  }
+  return papers;
+}
