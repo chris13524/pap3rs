0a0a1361d069ed1f2c2affbdbcc6257f0845ca5b
Chris Smith <chris@smith.xyz>
2022-07-24 16:23:46 -0400

    Fix local development

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 import Image from "next/image";
 
 const Logo: NextPage = () => {
-  return <Image src="logo.png" width={165} height={61} alt="Pap3rs" />;
+  return <Image src="/logo.png" width={165} height={61} alt="Pap3rs" />;
 };
 
 export default Logo;
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: './', // support IPFS hosting
+  // assetPrefix: './', // support IPFS hosting
   experimental: {
     images: {
       unoptimized: true,
