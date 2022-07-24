aed9a3941e0d44f46e56a07e7f566bdd00dbb5f0
Chris Smith <chris@smith.xyz>
2022-07-24 15:45:41 -0400

    Publish on Valist

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,9 +1,8 @@
-import { Title } from "@mantine/core";
 import { NextPage } from "next";
 import Image from "next/image";
 
 const Logo: NextPage = () => {
-  return <Image src="/logo.png" width={165} height={61} alt="Pap3rs" />;
+  return <Image src="logo.png" width={165} height={61} alt="Pap3rs" />;
 };
 
 export default Logo;
--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  assetPrefix: './', // support IPFS hosting
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
